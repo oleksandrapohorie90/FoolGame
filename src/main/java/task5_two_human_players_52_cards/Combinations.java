f8e96149d0e813a773df66c8f0d795c4b5de399e
oleksandrapohorie90 <oleksandra.pohorielova@gmail.com>
2024-12-16 19:16:44 -0500

    12/15/2024

---
+++ src/main/java/task5_two_human_players_52_cards/Combinations.java
@@ -0,0 +1,41 @@
+package task5_two_human_players_52_cards;
+
+public class Combinations {
+    /**
+     * Combinations:
+     * 1. Royal Flush (A♦ K♦ Q♦ J♦ T♦)
+     * The best hand possible, a royal flush consists of A, K, Q, J and 10, all of the same suit.
+     *
+     * 2. Straight Flush (T♥ 9♥ 8♥ 7♥ 6♥)
+     * Also very rare, a straight flush consists of any straight that is all the same suit.
+     *
+     * 3. Four-of-a-Kind (J♦ J♣ J♠ J♥ K♦)
+     * Four of a kind, or 'quads', consists of four cards of equal value along with another card known as a side card.
+     *
+     * 4. Full House (A♥ A♣ A♦ 9♠ 9♣)
+     * A full house consists of three cards of one value and two cards of another.
+     *
+     * 5. Flush (A♠ J♠ 8♠ 4♠ 3♠)
+     * A flush is a hand which has all cards of the same suit.
+     *
+     * 6. Straight (9♥ 8♠ 7♣ 6♦ 5♣)
+     * A straight has 5 cards of consecutive value that are not all the same suit.
+     *
+     * 7. Three-of-a-Kind (7♠ 7♦ 7♣ K♦ Q♣)
+     * Also known as 'trips', three of a kind is 3 cards of the same value and 2 side cards of different values.
+     *
+     * 8. Two-Pair (9♣ 9♦ 6♣ 6♠ Q♥)
+     * ​​Two pair consists of two cards of equal value, another two cards of equal value, and one extra card.
+     *
+     * 9. One-Pair (A♦ A♥ K♠ 9♦ 4♥)
+     * One pair consists of two cards of the same value, and three extra cards.
+     *
+     * 10. High Card (A♠ J♦ 8♣ 6♠ 2♥)
+     * High card is when you have five cards that do not interact with each other to make any of the above hands.
+     */
+
+
+
+
+
+}
---
+++ src/main/java/task5_two_human_players_52_cards/SimplifiedPokerDemo.java
@@ -0,0 +1,8 @@
+package task5_two_human_players_52_cards;
+
+public class SimplifiedPokerDemo {
+
+
+
+
+}
